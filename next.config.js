7a474585f4b87e71f6f73117c44d5e764985130a
matvel58 <Alexcool20152014@yandex.ru>
2026-02-24 21:12:56 +0300

    fix: hero image - use import, disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Avoid Vercel INVALID_IMAGE_OPTIMIZE_REQUEST
+  },
 };
 
 module.exports = nextConfig;
